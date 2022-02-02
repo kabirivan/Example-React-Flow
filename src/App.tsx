213aa205dd9ea63eaba1121580ab1d1d7eaa9c94
IVAN <aguas.xavier2013a@gmail.com>
2022-02-02 13:04:19 -0500

    review elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 import './App.css';
 import ReactFlow, { removeElements, addEdge, Connection, Edge, Elements, Position } from 'react-flow-renderer';
 
@@ -8,26 +8,43 @@ const initialElements = [
     type: 'input', // input node
     data: { label: 'Start Node' },
     draggable: false,
-    position: { x: 0, y: 50 },
+    position: { x: 500, y: 50 },
     sourcePosition: Position.Bottom,
   },
   // default node
   {
     id: '2',
-    type: 'output', 
+    type: 'output',
     // you can also pass a React component as a label
     data: { label: <div>Default Node</div> },
     draggable: true,
-    position: { x: 0, y: 150 },
+    position: { x: 500, y: 150 },
     sourcePosition: Position.Right,
   },
 ];
 
 
 function App() {
-  const [elements, setElements] = useState<a>(initialElements);
+  const [elements, setElements] = useState<Elements>(initialElements);
   const onElementsRemove = (elementsToRemove: Elements) => setElements((els) => removeElements(elementsToRemove, els));
   const onConnect = (params: Edge<any> | Connection) => setElements((els) => addEdge(params, els));
+  const yPos = useRef(0);
+
+  const addNode = useCallback(() => {
+    yPos.current += 50;
+    // ts-ignore
+    setElements((els) => {
+      console.log(els);
+      return [
+        ...els,
+        {
+          id: Math.random(),
+          position: { x: 100, y: yPos.current },
+          data: { label: "yo" }
+        }
+      ];
+    });
+  }, []);
 
   return (
     <div style={{ height: 1200 }}>
@@ -37,6 +54,8 @@ function App() {
         onConnect={onConnect}
         deleteKeyCode={46} /* 'delete'-key */
       />
+
+      <button onClick={addNode}>Add</button>
     </div>
   );
 }
